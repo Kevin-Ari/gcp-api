ba817cdda439b4ddb69ace6589ff902b0d9fdfb8
kevin-ari <dagnerdev@gmail.com>
2025-11-05 21:25:49 -0300

    nuevos usuarios

--- main.py
+++ main.py
@@ -15,20 +15,30 @@ class Usuario(BaseModel):
     email: str
     edad: int
     ciudad: str
+    telefono: int
 
 # Datos ficticios
 usuarios_db = [
-    Usuario(id=1, nombre="Ana García", email="ana.garcia@example.com", edad=28, ciudad="Buenos Aires"),
-    Usuario(id=2, nombre="Carlos López", email="carlos.lopez@example.com", edad=34, ciudad="Rosario"),
-    Usuario(id=3, nombre="Lucía Fernández", email="lucia.fernandez@example.com", edad=22, ciudad="Córdoba"),
-    Usuario(id=4, nombre="Mariano Torres", email="mariano.torres@example.com", edad=40, ciudad="Mendoza"),
-    Usuario(id=5, nombre="Valentina Díaz", email="valentina.diaz@example.com", edad=30, ciudad="La Plata"),
+    Usuario(id=1, nombre="Ana García", email="ana.garcia@example.com", edad=28, ciudad="Buenos Aires", telefono=11-6556-4534),
+    Usuario(id=2, nombre="Carlos López", email="carlos.lopez@example.com", edad=34, ciudad="Rosario", telefono=11-3456-6767),
+    Usuario(id=3, nombre="Lucía Fernández", email="lucia.fernandez@example.com", edad=22, ciudad="Córdoba", telefono=11-3455-4566),
+    Usuario(id=4, nombre="Mariano Torres", email="mariano.torres@example.com", edad=40, ciudad="Mendoza", telefono=11-5678-7677),
+    Usuario(id=5, nombre="Valentina Díaz", email="valentina.diaz@example.com", edad=30, ciudad="La Plata", telefono=11-6788-9999),
+    Usuario(id=6, nombre="Carlos Rodríguez", email="carlos.rodriguez@example.com", edad=35, ciudad="Córdoba", telefono=11-6788-8888),
+    Usuario(id=7, nombre="Ana Martínez", email="ana.martinez@example.com", edad=28, ciudad="Rosario", telefono=11-1111-1111),
+    Usuario(id=8, nombre="Diego Fernández", email="diego.fernandez@example.com", edad=42, ciudad="Tucumán", telefono=11-2345-4567),
+    Usuario(id=9, nombre="Lucía Sánchez", email="lucia.sanchez@example.com", edad=26, ciudad="Mar del Plata", telefono=11-4567-4567),
+    Usuario(id=10, nombre="Javier López", email="javier.lopez@example.com", edad=33, ciudad="Salta", telefono=11-4568-7899),
+    Usuario(id=11, nombre="Sofía Ramírez", email="sofia.ramirez@example.com", edad=29, ciudad="San Juan", telefono=11-2344-2344),
+    Usuario(id=12, nombre="Miguel Castro", email="miguel.castro@example.com", edad=45, ciudad="Santa Fe", telefono=11-2456-7654),
+    Usuario(id=13, nombre="Elena Morales", email="elena.morales@example.com", edad=31, ciudad="Corrientes", telefono=11-4567-8789),
+    Usuario(id=14, nombre="Andrés Ruiz", email="andres.ruiz@example.com", edad=38, ciudad="Neuquén", telefono=11-4567-7777)
 ]
 
 # Rutas
 @app.get("/")
 def root():
-    return {"mensaje": "Bienvenido a la API de usuarios ficticios"}
+    return {"mensaje": "Bienvenido a mi api de usuarios ficticios"}
 
 @app.get("/usuarios", response_model=List[Usuario])
 def listar_usuarios():
